0e53552cd4f3a8d7f81da021b7b92d60b007a3f0
Tomas KOVAC <office@ztk-comp.sk>
2021-02-04 18:22:45 +0100

    Create generate_points.sh

---
+++ generate_points.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+TOTAL="0"
+DIR="/opt/gsm/"
+
+cd ${DIR}
+
+for BTS in `ls ${DIR} | grep clf | grep -v txt | grep 2310`; do
+OP="0"
+ echo "var latLong = [{" > ${BTS}.txt.tmp
+ chown root. ${BTS} ${BTS}.txt.tmp
+ chmod 644 ${BTS} ${BTS}.txt.tmp
+ for SURADNICE in `cat ${DIR}/${BTS} | egrep -v "//|\+0.000" | grep "+" | cut -d ';' -f5,6 | tr -d '\+' | sort | uniq`; do
+  echo "\"lat\": `echo ${SURADNICE} | cut -d ';' -f1`," >> ${BTS}.txt.tmp
+  echo "\"lng\": `echo ${SURADNICE} | cut -d ';' -f2`" >> ${BTS}.txt.tmp
+  echo "}, {" >> ${BTS}.txt.tmp
+  let TOTAL++;
+  let OP++;
+ done
+ echo "\"lat\": 0.000000," >> ${BTS}.txt.tmp
+ echo "\"lng\": 0.000000" >> ${BTS}.txt.tmp
+ echo "}];" >> ${BTS}.txt.tmp
+ echo "${BTS} = ${OP}";
+ mv ${BTS}.txt.tmp ${DIR}${BTS}.txt
+done
+echo "Total: ${TOTAL}";
